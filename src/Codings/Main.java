0355ba226c222021ba58df7a4701eb47d584ef00
NurikSolto7007 <n.soltoev@gmail.com>
2019-12-27 21:46:38 +0600

    Initial commit

---
+++ src/Codings/Main.java
@@ -0,0 +1,17 @@
+package Codings;
+
+import java.security.PublicKey;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Hello World";
+        final String name2 = "Smile";
+        System.out.println(name + name2);
+        
+
+    }
+}
+
